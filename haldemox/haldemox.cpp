4229ada595348355e8cc32316f8a15c87be0990b
Li <victor.y.li@intel.com>
2016-07-19 20:36:52 -0700

    Wirte v2 to avoid creating all of instances at the initialization.

--- haldemox/haldemox.cpp
+++ haldemox/haldemox.cpp
@@ -21,9 +21,9 @@ int submit_command(HalBase *phal)
     return pxhal->submit_command();
 }
 
-int _tmain(int argc, _TCHAR* argv[])
+
+void test_v1()
 {
-    
     //Create instances for all supported platforms.
     //All instances are stored in a vector
     std::vector<HalBase *> platform_vector;
@@ -31,7 +31,7 @@ int _tmain(int argc, _TCHAR* argv[])
 
     //get the platfrom at run time
     GEN_PLATFORM running_platfrom = GEN11;
-    
+
     //use platform id to get the right instance
     //invoke derived class's interface 
     for (std::vector<HalBase *>::iterator it = platform_vector.begin();
@@ -50,6 +50,54 @@ int _tmain(int argc, _TCHAR* argv[])
             break;
         }
     }
+
+    return;
+}
+
+//v2: avoid creating all platform classes at the beginning.
+void init_v2(HalBase * &phal, GEN_PLATFORM running_platfrom)
+{
+    switch (running_platfrom)
+    {
+    case GEN9:
+        phal = new HalGen9;
+        break;
+
+    case GEN11:
+        phal = new HalGen11;
+        break;
+    }
+    return;
+}
+
+void test_v2()
+{
+    //get the platfrom at run time
+    GEN_PLATFORM running_platfrom = GEN11;
+
+    //Init Class according to the running platform
+    HalBase *phal = NULL;
+    init_v2(phal, running_platfrom);
+
+    //Invoke right template with running platform
+    switch (running_platfrom)
+    {
+    case GEN9:
+        submit_command<HalGen9>(phal);
+        break;
+
+    case GEN11:
+        submit_command<HalGen11>(phal);
+        break;
+    }
+
+    return;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    
+    test_v2();
 
 	return 0;
 }
